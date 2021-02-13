f5867b03cd390d9771a41b5f44bacd9afbbd3cca
sacode <coimbrasantos.d@gmail.com>
2021-02-13 00:12:39 -0300

    Code blocks save code lines

--- tp1/tp1.java
+++ tp1/tp1.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 
 public class Tp1 {
+	
+	public static Scanner scan = new Scanner(System.in);
 
 	public static Year year_list[] = {new Year(2011),new Year(2012),new Year(2013),new Year(2014),new Year(2015),new Year(2016),new Year(2017),new Year(2018),new Year(2019),new Year(2020)};
     
@@ -18,8 +20,6 @@ public class Tp1 {
     	public Year(int year) {
 			this.year = year;
 			this.isLeap = check_leap();
-			
-//			System.out.println("new year");
 			create_months();
 		}
     	
@@ -46,14 +46,15 @@ public class Tp1 {
     
     public static class Month{
     	int month;
-    	double
-    		min,
-    		max;
+    	double	min,
+    			max;
 
-    	boolean from_leap;
+    	boolean from_leap,
+    			temp_done;
     	
     	ArrayList<Day> days = new ArrayList<Day>();
-    	
+    	ArrayList<Day> maxs = new ArrayList<Day>();
+    	ArrayList<Day> mins = new ArrayList<Day>();
     	double median;
     	
     	public Month(int month) {
@@ -65,6 +66,7 @@ public class Tp1 {
     	public Month(int month, boolean from_leap) {
     		this.month = month;
     		this.from_leap = true;
+    		
 //    		System.out.println("new month"+this.month);
 			create_days();
     	}
@@ -96,17 +98,17 @@ public class Tp1 {
     	
     	public double get_median_temp() {
     		double sum = 0;
-
+    		
     		for (Day day : this.days){
-    		sum += day.day;
+    			sum += day.temp;
     		}
     		
-    		double total = sum / get_num_days();
+    		double total = sum / this.days.size();
     		
     		return total;
     	}
 		
-    	public double get_min_temp() {
+    	public ArrayList<Day> get_min_temp() {
     		int count =0;
     		
     		for (Day day : this.days){
@@ -118,22 +120,33 @@ public class Tp1 {
         		}
     			count++;
         	}
-    		return this.min;
-		}
-		
-		public double get_max_temp() {
-    		int count =0;
-    		for (Day day : this.days){
-        		if (count==0)
-        			this.max = day.temp;
-        		else if(day.temp>max)
-        			this.max = day.temp;
-        			
-    			count++;
-        	}
-    		return this.max;
+    		
+    		for (Day day : this.days) {
+    			if (day.temp == this.min)
+    				this.mins.add(day);
+    		}
+    		return this.mins;
 		}
     	
+    	public ArrayList<Day> get_max_temp() {
+    		int count =0;
+    		
+    		for (Day day : this.days){
+        		if (count==0) {
+        			this.max = day.temp;
+        		}
+        		else if(day.temp>max) {
+        			this.max = day.temp;
+        		}
+    			count++;
+        	}
+    		
+    		for (Day day : this.days) {
+    			if (day.temp == this.max)
+    				this.maxs.add(day);
+    		}
+    		return this.maxs;
+		}
     }
     
     public static class Day{
@@ -152,13 +165,98 @@ public class Tp1 {
     	}
     	
     	public void take_temp() {
-    		Scanner scan = new Scanner(System.in);
+    		scan = new Scanner(System.in);
     		System.out.println("Digite a temperatura do dia " + this.day);
     		this.temp = scan.nextInt();
+    		
     		
     	}
     }
     
+    public static ArrayList<Integer> please_scan(){
+    	ArrayList<Integer> lista = new ArrayList<Integer>();
+    	int this_year = 0;
+    	System.out.println("Digite o ano(2011~2020):");
+ 		try{
+ 			lista.add(scan.nextInt());
+ 		}catch(Exception e){
+ 			System.out.println("Ano não encontrado.");
+ 			callback_menu();
+ 		}
+ 		System.out.println("Digite o mês(1~12):");
+ 		try{
+ 			lista.add(scan.nextInt());
+ 		}catch(Exception e){
+ 			System.out.println("Mês não encontrado.");
+ 			callback_menu();
+ 		}
+
+		if(this_year < 2011 || this_year > 2020) {
+ 			System.out.println("Ano não encontrado.");
+ 			callback_menu();
+ 		}
+		
+		return lista;
+    }
+    
+    public static void case2(int user_year, int user_month) {
+    	
+    	
+    	for(Year year :year_list) {
+ 			if(user_year == year.year) {
+ 				
+ 				try{
+	 				if (year.months[user_month - 1].temp_done) {
+	 					
+	 					System.out.println("Temperatura média " + user_month + "/" + year.year+ ": "+year.months[user_month - 1].get_median_temp() + "°C");
+	 					
+	 				}else {
+	 					
+	 				}
+ 				}
+ 				catch(Exception e){
+ 					System.out.println("Mês não encontrado.");
+ 					callback_menu();
+ 				}
+ 				callback_menu();
+ 			}
+ 		}
+    }
+    public static void case3(int user_year, int user_month) {
+    	
+    	for(Year year :year_list) {
+ 			if(user_year == year.year) {
+ 				if (year.months[user_month - 1].temp_done) {
+ 					year.months[user_month - 1].get_min_temp().forEach((day) -> System.out.println("O dia " + day.day + " foi o mais frio de " +user_month +"/" + year.year +" "+"com "+ day.temp +"°C"));
+
+ 				}else {
+ 					System.out.println("Não há dados suficientes.");
+ 					callback_menu();
+ 				}
+ 				
+ 			}
+ 		}
+    }
+    public static void case4(int user_year, int user_month) {
+    	
+    	for(Year year :year_list) {
+ 			if(user_year == year.year) {
+ 				if (year.months[user_month - 1].temp_done) {
+ 					year.months[user_month - 1].get_max_temp().forEach((day) -> System.out.println("O dia " + day.day + " foi o mais quente de " +user_month +"/" + year.year +" "+"com "+ day.temp +"°C"));
+
+ 				}else {
+ 					System.out.println("Não há dados suficientes.");
+ 					callback_menu();
+ 				}
+ 				
+ 			}
+ 		}
+    }
+    
+    public static void callback_menu() {
+    	System.out.println("\nFim. Voltando ao menu...");
+    	cli_menu();
+    }
     
 	 public static void cli_menu(){
 		 
@@ -169,109 +267,91 @@ public class Tp1 {
 		 System.out.println("4. Cálculo da temperatura máxima de um mês");
 		 System.out.println("5. Relatório meteorológico completo\n");
 		 
-		 Scanner scan = new Scanner(System.in);
-		 int option = scan.nextInt();
-		 int this_year;
+		 ArrayList<Integer> lista = please_scan();
+		 int option = scan.nextInt(),
+			 user_year = lista.get(0),
+			 user_month = lista.get(1);
+
 		 switch(option) {
 		 	
-		 	case 1:
-		 		System.out.println("1:\n");
-		 		System.out.println("Digite o ano(2011~2020):");
-		 		this_year = scan.nextInt();
+		 	case 1: //		 		make temperature entries for all of the month's days and save that data
+		 		
+		 		System.out.println("Você escolheu a opção 1\n");
+		 		
+		 		
 		 		for(Year year :year_list) {
-		 			if(this_year == year.year) {
-		 				System.out.println("Digite o mês(1~12):\n");
-		 				int this_month = scan.nextInt();
+		 			if(user_year == year.year) {
+		 				
 		 				try{
-		 					System.out.println(year.months[this_month - 1].month);
+			 				if (year.months[user_month - 1].temp_done) {
+			 					System.out.println("Mẽs já registrado, deseja sobrescrever? Y/N");
+			 					String str = scan.next();
+			 					if (str.equalsIgnoreCase("y")) {
+			 						for(int i=0;i<year.months[user_month - 1].get_num_days(); i++) {
+				 						year.months[user_month - 1].days.get(i).take_temp();
+				 					}
+				 					year.months[user_month - 1].temp_done = true;
+				 					System.out.println("Temperaturas registradas com sucesso");
+				 					break;
+			 					}
+			 					else {
+			 						System.out.println("Não sobrescrever.");
+			 						break;
+			 					}
+			 					
+			 				}else {
+			 					for(int i=0;i<year.months[user_month - 1].get_num_days(); i++) {
+			 						year.months[user_month - 1].days.get(i).take_temp();
+			 					}
+			 				}
+		 					year.months[user_month - 1].temp_done = true;
+		 					System.out.println("Temperaturas registradas com sucesso");
 		 				}
 		 				catch(Exception e){
-		 					System.out.println("Mês não encontrado. Voltando ao início");;
+		 					System.out.println("Mês não encontrado.");
+		 					break;
 		 				}
-		 			}
-		 			
-		 			else {
-		 				System.out.println("Ano não encontrado. Voltando ao menu...");
 		 				break;
 		 			}
 		 		}
-//		 		System.out.println("Erro. Voltando ao ínicio...\n");
-//		 		cli_menu();
+
+		 		break;
+
+		 	case 2: //		 		calculate median temperature of a specific month of a specific year
 		 		
+		 		System.out.println("Você escolheu a opção 2\n");
 		 		
+		 		case2(user_year, user_month);
+		 		break;
+
+		 	case 3: //		 		calculate min temperature of a specific month of a specific year and say which day(s)
 		 		
+		 		System.out.println("Você escolheu a opção 3\n");
 		 		
-//		 		make temperature entries for all of the month's days and save that data
+		 		case3(user_year, user_month);
 		 		break;
 		 	
 		 	
-		 	case 2:
+		 	case 4: //		 		calculate max temperature of a specific month of a specific year and say which day(s)
+		 		System.out.println("Você escolheu a opção 4\n");
+
+		 		case4(user_year, user_month);
 		 		
-//		 		calculate median temperature of a specific month of a specific year
 		 		break;
 		 	
-		 	
-		 	case 3:
-		 		
-//		 		calculate min temperature of month
-		 		break;
-		 	
-		 	
-		 	case 4:
-		 		
-//		 		calculate max temperature of month
-		 		break;
 		 	case 5:
+		 		System.out.println("Você escolheu a opção 5\n");
 		 		
-//		 		full meteorologic report:
-//		 			median temp for each dom
-//		 			median temp of month
-//		 			min temp of month
-//		 			max temp of month
-		 		break;
-		 	
-		 	
+		 		case2(user_year, user_month);
+		 		case3(user_year, user_month);
+		 		case4(user_year, user_month);
 		 	default:
-		 		System.out.println("chose wrong");
+		 		System.out.println("Erro.");
 		 		break;
 		 }
 		 
-		 System.out.println("\nDone. Looping back to menu...");
-		 
-//		 Thread.sleep(2);
-		 cli_menu();
+		 callback_menu();
 	 }
-
-	 
-	public static Year find_year(int n_year){
-		for (Year year : year_list){
-			if (year.year == n_year)
-			return year;
-			
-		}
-		return new Year(0);
-	}
-	
-	public static Month find_month(int n_month) {
-		return new Month(0);
-	}
-	
-	public static void case1() {
-		
-	}
-	public static void case2() {
-			
-		}
-	public static void case3() {
-		
-	}
-	public static void case4() {
-		
-	}
-	public static void case5() {
-		
-	}
-
 	
 	
 	public static void main(String[] args){
