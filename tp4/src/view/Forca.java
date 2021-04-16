d7163cb4562e5eef4e444b8166764fe04062faea
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-16 15:47:40 -0300

    Botoes das letras

--- tp4/src/view/Forca.java
+++ tp4/src/view/Forca.java
@@ -1,8 +1,13 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 public class Forca extends Tela{
 	public Forca() {
-		
+		botoes_letras();
 	}
 	
 	public String botao_dica() {
@@ -10,6 +15,217 @@ public class Forca extends Tela{
 		
 		
 		return dica;
+	}
+	
+	public void botoes_letras() {
+		JButton btnNewButton = new JButton("A");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(99, 170, 55, 25);
+		
+		this.get_tela().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("B");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1.setBounds(154, 170, 55, 25);
+		this.get_tela().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("C");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2.setBounds(209, 170, 55, 25);
+		this.get_tela().add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("D");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_3.setBounds(264, 170, 55, 25);
+		this.get_tela().add(btnNewButton_3);
+		
+		JButton btnNewButton_4 = new JButton("E");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_4.setBounds(319, 170, 55, 25);
+		this.get_tela().add(btnNewButton_4);
+		
+		JButton btnNewButton_5 = new JButton("F");
+		btnNewButton_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_5.setBounds(374, 170, 55, 25);
+		this.get_tela().add(btnNewButton_5);
+		
+		JButton btnNewButton_6 = new JButton("G");
+		btnNewButton_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_6.setBounds(429, 170, 55, 25);
+		this.get_tela().add(btnNewButton_6);
+		
+		JButton btnNewButton_7 = new JButton("H");
+		btnNewButton_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_7.setBounds(99, 207, 55, 25);
+		this.get_tela().add(btnNewButton_7);
+		
+		JButton btnNewButton_1_1 = new JButton("I");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1_1.setBounds(154, 207, 55, 25);
+		this.get_tela().add(btnNewButton_1_1);
+		
+		JButton btnNewButton_2_1 = new JButton("J");
+		btnNewButton_2_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_1.setBounds(209, 207, 55, 25);
+		this.get_tela().add(btnNewButton_2_1);
+		
+		JButton btnNewButton_3_1 = new JButton("K");
+		btnNewButton_3_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_3_1.setBounds(264, 207, 55, 25);
+		this.get_tela().add(btnNewButton_3_1);
+		
+		JButton btnNewButton_4_1 = new JButton("L");
+		btnNewButton_4_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_4_1.setBounds(319, 207, 55, 25);
+		this.get_tela().add(btnNewButton_4_1);
+		
+		JButton btnNewButton_5_1 = new JButton("M");
+		btnNewButton_5_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_5_1.setBounds(374, 207, 55, 25);
+		this.get_tela().add(btnNewButton_5_1);
+		
+		JButton btnNewButton_6_1 = new JButton("N");
+		btnNewButton_6_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_6_1.setBounds(429, 207, 55, 25);
+		this.get_tela().add(btnNewButton_6_1);
+		
+		JButton btnNewButton_8 = new JButton("O");
+		btnNewButton_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_8.setBounds(99, 244, 55, 25);
+		this.get_tela().add(btnNewButton_8);
+		
+		JButton btnNewButton_1_2 = new JButton("P");
+		btnNewButton_1_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1_2.setBounds(154, 244, 55, 25);
+		this.get_tela().add(btnNewButton_1_2);
+		
+		JButton btnNewButton_2_2 = new JButton("Q");
+		btnNewButton_2_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_2.setBounds(209, 244, 55, 25);
+		this.get_tela().add(btnNewButton_2_2);
+		
+		JButton btnNewButton_3_2 = new JButton("R");
+		btnNewButton_3_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_3_2.setBounds(264, 244, 55, 25);
+		this.get_tela().add(btnNewButton_3_2);
+		
+		JButton btnNewButton_4_2 = new JButton("S");
+		btnNewButton_4_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_4_2.setBounds(319, 244, 55, 25);
+		this.get_tela().add(btnNewButton_4_2);
+		
+		JButton btnNewButton_5_2 = new JButton("T");
+		btnNewButton_5_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_5_2.setBounds(374, 244, 55, 25);
+		this.get_tela().add(btnNewButton_5_2);
+		
+		JButton btnNewButton_6_2 = new JButton("U");
+		btnNewButton_6_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_6_2.setBounds(429, 244, 55, 25);
+		this.get_tela().add(btnNewButton_6_2);
+		
+		JButton btnNewButton_9 = new JButton("V");
+		btnNewButton_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_9.setBounds(121, 281, 54, 25);
+		this.get_tela().add(btnNewButton_9);
+		
+		JButton btnNewButton_1_3 = new JButton("W");
+		btnNewButton_1_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_1_3.setBounds(187, 281, 55, 25);
+		this.get_tela().add(btnNewButton_1_3);
+		
+		JButton btnNewButton_2_3 = new JButton("X");
+		btnNewButton_2_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_3.setBounds(254, 281, 55, 25);
+		this.get_tela().add(btnNewButton_2_3);
+		
+		JButton btnNewButton_2_3_1 = new JButton("Y");
+		btnNewButton_2_3_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_3_1.setBounds(319, 281, 55, 25);
+		this.get_tela().add(btnNewButton_2_3_1);
+		
+		JButton btnNewButton_2_3_2 = new JButton("Z");
+		btnNewButton_2_3_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton_2_3_2.setBounds(384, 281, 55, 25);
+		this.get_tela().add(btnNewButton_2_3_2);
 	}
 	
 	public String botao_letra(String letra) {
