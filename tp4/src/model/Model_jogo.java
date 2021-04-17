44766eaa7df34f7f910e5741f1e2d83260f88620
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-16 23:28:47 -0300

    update package model

--- tp4/src/model/Model_jogo.java
+++ tp4/src/model/Model_jogo.java
@@ -10,6 +10,7 @@ import java.io.FileReader;
 import java.io.LineNumberReader;
 import java.util.Random;
 import java.util.Scanner;
+import control.*;
 
 
 public class Model_jogo {
@@ -59,18 +60,8 @@ public class Model_jogo {
 //		
 //		return x;
 //	}
-	
-	
-	
-//	public static String get_palavra_da_vez() {
-//		
-//	}
-	
-	public static void ler_database() {
-		
-	}
-	
-	public static int get_lista_palavras_size() throws Exception {
+
+	public static int tamanho_lista_palavras() throws Exception {
 		LineNumberReader leitorLinhas = new LineNumberReader(new FileReader(DATABASE));
 		leitorLinhas.skip(Long.MAX_VALUE);
 		int quantPalavras = leitorLinhas.getLineNumber() + 1;
@@ -94,17 +85,31 @@ public class Model_jogo {
 		
 	}
 	
-	public static String get_palavra_sorteada() {
+	public static void sortear_palavra() {
 		
 		int indiceSorteado = random.nextInt(lista_palavras_chave.size());
 		
 		String palavra_sorteada = lista_palavras_chave.get(indiceSorteado);
 		lista_palavras_chave.remove(indiceSorteado);
-		return palavra_sorteada;
-
+		palavra_da_vez = palavra_sorteada;
+		Controller_jogo.mostrar_palavra();
 	}
 	
-	public static void rodada(String letra, String palavra) {
+
+	
+	public static String p_da_vez() {
+		return palavra_da_vez;
+	}
+	
+	public static void checar_letra(char letra, String palavra) {
+		for(int i=0;i<palavra.length(); i++) {
+			if (palavra.charAt(i)==letra) {
+				
+			}
+		}
+	}
+	
+	public static void checa_estado() {
 		
 	}
 	
