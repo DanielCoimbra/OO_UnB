9179b9ee99ce74f8817015fe8e028a46a5cdc6c2
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-28 21:26:46 -0300

    JavaDOC Tela.java

--- tp4/src/view/Tela.java
+++ tp4/src/view/Tela.java
@@ -13,7 +13,9 @@ public class Tela{
 	public Tela() {
 		bootstrap();
 	}
-	
+	/**
+	 * inicia uma tela
+	 */
 	public void bootstrap() {
 		this.tela = new JFrame();
 		this.tela.setLocation(500,300);
@@ -26,19 +28,26 @@ public class Tela{
 		botao_menu();
 
 	}
+	/**
+	 * retorna a tela da vez
+	 * @return retorna a tela
+	 */
 	
-	public JFrame get_tela() {
+		public JFrame get_tela() {
 		return this.tela;
 	}
-	
-	public void botao_sair() {
-		
-	}
+
+	/**
+	 * navega o jogo pro menu
+	 * @throws Exception
+	 */
 	public void clica_botao_menu() throws Exception {
 		this.get_tela().setVisible(false);
 		Controller_jogo.navegar_tela(0);
 	}
-	
+	/**
+	 * coloca o botão menu
+	 */
 	public void botao_menu() {
 		JButton b=new JButton("MENU");
 		b.setBounds(130,100,100, 40);//x axis, y axis, width, height
