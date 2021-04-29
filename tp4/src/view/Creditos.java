1763415901721c809b9d38064963df73159d7994
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-28 21:16:17 -0300

    JavaDOC Creditos.java

--- tp4/src/view/Creditos.java
+++ tp4/src/view/Creditos.java
@@ -1,10 +1,18 @@
 package view;
 import javax.swing.*;
+
+/**
+	 * Tela de Créditos do Jogo
+	 * @author Daniel Coimbra
+	 * @versão 1.0 (Abril 2021)
+	 */
 public class Creditos extends Tela{
 	public Creditos() {
 		mostrar_creditos();
 	}
-	
+	/**
+	 * coloca a label de creditos do jogo
+	 */
 	public void mostrar_creditos(){		
 		
 		JLabel lblTeste = new JLabel("by Donão e Pedrão");
