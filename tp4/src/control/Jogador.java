327923fb92475a8a9d17ecfebc767e73373c1b7c
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-28 21:28:32 -0300

    JavaDoc Jogador.java

--- tp4/src/control/Jogador.java
+++ tp4/src/control/Jogador.java
@@ -1,5 +1,10 @@
 package control;
 
+/**
+ * Classe responsável pelos dados de um jogador
+ * @author Daniel Coimbra
+ * @versão 1.0 (Abril 2021)
+ */
 public class Jogador {
 	private String player_name;
 	public int score;
@@ -8,26 +13,37 @@ public class Jogador {
 	public Jogador() {
 		
 	}
-	
+	/**
+	 * 
+	 * @return nome do jogador
+	 */
 	public String get_name() {
 		return player_name;
 	}
-	
+	/**
+	 * registra o nome do jogador
+	 * @param name
+	 */
 	public void set_name(String name) {
 		player_name= name;
 	}
-
-	public void reset_lives() {
-		
-	}
-	
+	/**
+	 * 
+	 * @return player score
+	 */
 	public int get_score() {
 		return this.score;
 	}
-	
+	/**
+	 * diminui um de vida do jogador
+	 */
 	public void perder_1_de_vida(){
 		this.vidas -= 1;
 	}
+	/**
+	 * 
+	 * @return vidas do jogador em tempo de execucao
+	 */
 	public int vida() {
 		return this.vidas;
 	}
