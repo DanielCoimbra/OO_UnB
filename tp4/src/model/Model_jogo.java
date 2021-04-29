7c1ce7ccb7f73ae07ff6634a7bdc56995d7035ea
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-04-28 21:11:57 -0300

    JavaDOC Model_jogo.java

--- tp4/src/model/Model_jogo.java
+++ tp4/src/model/Model_jogo.java
@@ -17,7 +17,11 @@ import javax.swing.JOptionPane;
 
 import control.*;
 import view.Forca;
-
+/**
+ * Classe responsável pelo tratamento dos dados do jogo.
+ * @author Daniel Coimbra
+ * @versão 1.0 (Abril 2021)
+ */
 public class Model_jogo {
 	private static ArrayList<String> lista_palavras_chave = new ArrayList<String>();
 	public static ArrayList<Character> utilizadas = new ArrayList<Character>();
@@ -31,14 +35,29 @@ public class Model_jogo {
 	private static JLabel label_vidas = new JLabel();
 	public static int contador_acertos; //PUBLICO **************************************************************************************
 	
+	/**
+	 * Reseta vidas do jogador
+	 * 
+	 */
+	
 	public static void reset_j_lives() {
 		j.vidas = 5;
 	}
 	
+	/**
+	 * Retorna o numero de vidas do jogador
+	 * @return int com valor da vida do jogador
+	 * 
+	 */
 	public static int j_lives() {
 		return j.vidas;
 	}
 
+	/**
+	 * insere o index de uma letra acertada para referência futura
+	 * @return i 
+	 * 
+	 */
 	public static void acerto(int i) {
 		contador_acertos++;
 		
@@ -47,7 +66,10 @@ public class Model_jogo {
 			ganhou();
 		}
 	}
-	
+	/**
+	 * diminui a vida dessa rodada
+	 * 
+	 */
 	public static void erro(){
 		j.vidas--;
 		if (j.vidas < 0) {
@@ -55,18 +77,27 @@ public class Model_jogo {
 		}else
 			Controller_jogo.muda_vidas();
 		}
-	
+	/**
+	 * aumenta o score do jogador em 1
+	 */
 	public static void ganhou(){
 		Controller_jogo.ganhou_rodada();
 		j.score++;
 		estado="ganhou";
 
 	}
-	
+	/**
+	 * navega o jogador à tela de game over
+	 * @throws Exception
+	 */
 	public static void perdeu() throws Exception {
 		Controller_jogo.perdeu();
 	}
-
+	/**
+	 * retona o tamanho da lista de palavras disponiveis em tempo de execucao
+	 * @return int quant_palavras
+	 * @throws Exception
+	 */
 	public static int tamanho_lista_palavras() throws Exception {
 		LineNumberReader leitorLinhas = new LineNumberReader(new FileReader(DATABASE));
 		leitorLinhas.skip(Long.MAX_VALUE);
@@ -76,7 +107,10 @@ public class Model_jogo {
 		return quantPalavras;
 
 	}
-	
+	/**
+	 * reseta a lista das palavras disponíveis em jogo 
+	 * @throws Exception
+	 */
 	public static void reseta_lista_palavras() throws Exception {
 		BufferedReader leitorArquivo = new BufferedReader(new FileReader(DATABASE));
 		String linhaLida;
@@ -89,7 +123,9 @@ public class Model_jogo {
 		leitorArquivo.close();
 
 	}
-	
+	/**
+	 * sorteia a palavra para uma rodada
+	 */
 	public static void sortear_palavra() {
 		
 		int indiceSorteado = random.nextInt(lista_palavras_chave.size());
@@ -103,11 +139,17 @@ public class Model_jogo {
 	}
 	
 
-	
+	/**
+	 * retorna a palavra sorteada nesse round
+	 * @return palavra da vez
+	 */
 	public static String p_da_vez() {
 		return palavra_da_vez;
 	}
-	
+	/**
+	 * checa se a letra clicada pertence à palavra e direciona o jogo a partir disso
+	 * @param letra
+	 */
 	public static void checar_letra(char letra){
 		
 		index_list.clear();
@@ -132,14 +174,27 @@ public class Model_jogo {
 		Controller_jogo.utilizadas(letra);
 	}
 	
+	/**
+	 * da acesso a lista de labels dos char da palavra
+	 * auxilia os underlines virarem a letra correta durante o jogo
+	 * @return lista de labels
+	 */
 	public static ArrayList<JLabel> get_lista_label(){
 		return label_list;
 	}
-	
+	/**
+	 * da acesso ao JLabel da classe
+	 * retorna JLabel
+	 * @return 
+	 */
 	public static JLabel get_label_vidas() {
 		return label_vidas;
 	}
-	
+	/**
+	 * adiciona palavra à database
+	 * @throws IOException
+	 * 
+	 */
 	public static void add_palavra() throws IOException {
 		 FileWriter fw = new FileWriter(DATABASE, true);
 		    BufferedWriter bw = new BufferedWriter(fw);
@@ -149,20 +204,17 @@ public class Model_jogo {
 		    bw.write(algo.toUpperCase());
 		    bw.close();
 	}
-	
-	public static Boolean checa_utilizadas(char letra) {
-		for(Character char_: Model_jogo.utilizadas){
-			if (char_ == letra) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
+
+	/**
+	 * retorna score do jogador
+	 * @return score do jogador
+	 */
 	public static int get_score() {
 		return j.score;
 	}
-	
+	/**
+	 * reseta o score do jogador
+	 */
 	public static void reset_score() {
 		j.score = 0;
 	}
